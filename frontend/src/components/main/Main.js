4d853e966ca9cf182270c8db4a69fe9cd72d2ffb
Mike@server <mikethomas84@gmail.com>
2018-05-15 20:20:27 +0100

    basic components and structure

---
+++ frontend/src/components/main/Main.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Main.css";
+
+const Main = () => (
+  <div id="main">
+    main
+  </div>
+);
+
+export default Main;
---
+++ frontend/src/components/side/Side.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Side.css";
+
+const Side = () => (
+  <div id="side">
+    side
+  </div>
+);
+
+export default Side;
